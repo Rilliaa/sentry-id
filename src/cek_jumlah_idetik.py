1a4d8ad3fdc5282feb1e8865ed113bd2449c3be1
Rio Liando <166204479+Rilliaa@users.noreply.github.com>
2025-09-22 15:32:29 +0700

    Update cek_jumlah_idetik.py

--- src/cek_jumlah_idetik.py
+++ src/cek_jumlah_idetik.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # --- 1. Load dataset ---
-df = pd.read_csv("<CHANNEL_NAME>.csv")  
+df = pd.read_csv("dataset.csv")  #file dataset 
 
 # --- 2. Cek duplikat berdasarkan kolom Comment ---
 duplicates = df[df.duplicated(subset=["message"], keep=False)]  
@@ -14,3 +14,4 @@ duplicates.to_csv("kaggle_duplicates.csv", index=False, encoding="utf-8")
 print(f"Total data awal: {len(df)}")
 print(f"Data unik tersisa: {len(clean)}")
 print(f"Data duplikat ditemukan: {len(duplicates)} (sudah dipisahkan ke kaggle_duplicates.csv)")
+
